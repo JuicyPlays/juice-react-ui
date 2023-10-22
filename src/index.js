4ed24023b0b72f2a7df153696f57e5ca8f4d3101
nikhil-tammana <37557269+nikhil-tammana@users.noreply.github.com>
2023-10-22 00:32:47 -0500

    feat: initial commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <App/>
 );
-
-reportWebVitals(sendToVercelAnalytics);
